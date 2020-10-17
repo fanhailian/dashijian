b5e609b6f4196d673f1c25287da08c96ea37ffa3
itheima@qq.com <3006@.qq.com>
2020-10-17 19:26:41 +0800

    第一次提交所有文件

---
+++ assets/js/longin.js
@@ -0,0 +1,11 @@
+$(function () {
+    //注册点击事件
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show();
+    })
+    $('#.link_reg').on("click", function () {
+        $('.login-box').show()
+        $('.reg-box').hide();
+    })
+})
